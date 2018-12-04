0e6a49a04a0a1ac949960ee195ab5495148206cd
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2018-12-04 07:07:35 -0500

    Action application demo.

--- src/main/java/org/nasdanika/html/springbootdemo/HomeController.java
+++ src/main/java/org/nasdanika/html/springbootdemo/HomeController.java
@@ -1,16 +1,26 @@
 package org.nasdanika.html.springbootdemo;
 
-import org.json.JSONArray;
+import java.net.URL;
+
 import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.nasdanika.html.Select;
 import org.nasdanika.html.Tag;
 import org.nasdanika.html.app.Application;
+import org.nasdanika.html.app.ApplicationBuilder;
+import org.nasdanika.html.app.ViewGenerator;
+import org.nasdanika.html.app.impl.ActionApplicationBuilder;
 import org.nasdanika.html.app.impl.BootstrapContainerApplication;
+import org.nasdanika.html.app.impl.ContentAction;
+import org.nasdanika.html.bootstrap.BootstrapFactory;
 import org.nasdanika.html.bootstrap.Color;
+import org.nasdanika.html.bootstrap.Container;
+import org.nasdanika.html.bootstrap.InputGroup;
+import org.nasdanika.html.bootstrap.Placement;
 import org.nasdanika.html.bootstrap.Theme;
 import org.nasdanika.html.fontawesome.FontAwesomeFactory;
-import org.nasdanika.html.jstree.JsTreeContextMenuItem;
 import org.nasdanika.html.jstree.JsTreeFactory;
-import org.nasdanika.html.jstree.JsTreeNode;
+import org.nasdanika.html.knockout.KnockoutFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,13 +41,56 @@ public class HomeController {
 			}
 			
 		}) {
-			Tag treeContainer = app.getHTMLPage().getFactory().div();
 			app.header("Header")
 				.navigationBar("Navigation bar")
 				.navigationPanel("Navigation panel")
 				.content("Content")
 				.footer("Footer");
 			
+			return app.toString();
+		}
+	}
+	
+	@RequestMapping("/action.html")
+	public String actionApp() throws Exception {
+		JSONObject json = new JSONObject(new JSONTokener(new URL("https://www.nasdanika.org/products/html/2.0.0-SNAPSHOT/test-dumps/app/action/action.json").openStream()));
+		ContentAction contentAction = new ContentAction(json.toMap());
+		try (Application app = new BootstrapContainerApplication(Theme.Default) {
+			
+			{
+				header.background(Color.PRIMARY);
+				navigationBar.background(Color.LIGHT).text().color(Color.DARK);
+				
+				footer.background(Color.SECONDARY);
+				
+				navigationPanel.widthAuto().border(Color.DEFAULT, Placement.RIGHT);
+				contentRow.toHTMLElement().style("min-height", "500px");
+				container.border(Color.DEFAULT).margin().top(1);
+
+				// Theme select at the bottom for experimentation.
+				BootstrapFactory factory = BootstrapFactory.INSTANCE;
+				Select select = factory.themeSelect(Theme.Default);
+				InputGroup selectInputGroup = factory.inputGroup();
+				selectInputGroup.prepend("Select Bootstrap theme");
+				selectInputGroup.input(select);
+				Container themeSelectorContainer = factory.container();
+				themeSelectorContainer.row().col(selectInputGroup).margin().top(2);
+				getHTMLPage().body(themeSelectorContainer);
+								
+				FontAwesomeFactory.INSTANCE.cdn(getHTMLPage());
+				JsTreeFactory.INSTANCE.cdn(getHTMLPage());
+				KnockoutFactory.INSTANCE.cdn(getHTMLPage());
+				
+			}
+			
+		}) {
+			ApplicationBuilder appBuilder = new ActionApplicationBuilder(contentAction.getChildren().get(0).getChildren().get(0)) {
+				@Override
+				protected Object generateHeader(ViewGenerator viewGenerator, Object result) {
+					return ((Tag) super.generateHeader(viewGenerator, result)).addClass("text-dark", "text-decoration: none");
+				}
+			};							
+			appBuilder.build(app);
 			return app.toString();
 		}
 	}
